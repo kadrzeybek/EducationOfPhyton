42da771a7a8f15e4e36febf4cebc1762ed8456dd
kadrzeybek <119623139+kadrzeybek@users.noreply.github.com>
2023-06-18 22:30:52 +0300

    Add files via upload

---
+++ fn_input.py
@@ -0,0 +1,7 @@
+'''You can request a value from the user using the "input" function,
+but if you do not convert any value you want from the user,
+it will be saved as a string.
+'''
+
+x= input("Enter a number:")
+print("Entered number is",x)
---
+++ fn_type.py
@@ -0,0 +1,13 @@
+#You can find out the type of value you are using using the "type" function
+
+x=12
+print(type(x))
+
+y=12.5
+print(type(y))
+
+z=True
+print(type(z))
+
+p="Hello world"
+print(type(p))
