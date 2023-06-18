f7c8fb70cc58c2c4956412fd24ac9aed205b9731
kadrzeybek <119623139+kadrzeybek@users.noreply.github.com>
2023-06-18 23:54:38 +0300

    Add files via upload

---
+++ arithmetic_operators.py
@@ -0,0 +1,9 @@
+sayi1 = 6
+sayi2 = 4
+print("{} + {} = {}".format(sayi1, sayi2, sayi1 + sayi2))
+print("{} - {} = {}".format(sayi1, sayi2, sayi1 - sayi2))
+print("{} / {} = {}".format(sayi1, sayi2, sayi1 / sayi2))
+print("{} * {} = {}".format(sayi1, sayi2, sayi1 * sayi2))
+print("{} ** {} = {}".format(sayi1, sayi2, sayi1 ** sayi2))
+print("{} % {} = {}".format(sayi1, sayi2, sayi1 % sayi2))
+print("{} // {} = {}".format(sayi1, sayi2, sayi1 // sayi2))
